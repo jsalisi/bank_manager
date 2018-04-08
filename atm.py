34644c89e803dc6c3fa3d50719906c096d7f7350
jsalisi <jsalisi@gmail.com>
2018-04-08 13:48:20 -0700

    Included import statements in application files

--- atm.py
+++ atm.py
@@ -1,1 +1,4 @@
 # This is the file that runs the ATM
+
+from controller.atm_controller import *
+from views.atm_interface import *
--- cli.py
+++ cli.py
@@ -1,1 +1,4 @@
 # This is the file that runs the Command Line Interface
+
+from controller.cmd_controller import *
+from views.cmd_interface import *
