84ab02f51e5a1b1489abafcdb9954c3d26ab3776
jsalisi <jsalisi@gmail.com>
2018-04-10 20:15:36 -0700

    Some menus displaying now

--- controller/cmd_controller/cmd_controller.py
+++ controller/cmd_controller/cmd_controller.py
@@ -1,11 +1,56 @@
 # This will be the file for the CLI controller
 
-from views.cmd_interface.cmd_views import CommandLinePrompts
+from views.cmd_interface.cmd_views import CommandLinePrompts as prompts
+
 
 class CommandLineController:
 
     def __init__(self):
-        self.view = CommandLinePrompts()
+        self.view = prompts()
+
+    def login(self):
+        logged_in = False
+        while logged_in is False:
+            id = self.view.login_id()
+            passwd = self.view.login_passwd()
+            print(id, passwd)
+
+            if int(id) == 12345 and int(passwd) == 12345:
+                logged_in = True
+                return True
 
     def run(self):
-        print('running')
+        try:
+            # Login
+            logged_in = self.login()
+
+            if logged_in is True:
+                p1 = self.view.main_prompt()
+
+                # Managing Users
+                if p1 == 1:
+                    p2 = self.view.manage_users()
+
+                    # Configuring Users
+                    if p2 == 1:
+                        u_id = self.view.user_id()
+                        c_user = self.view.config_user(u_id)
+                    
+                    # Adding User
+                    elif p2 == 2:
+                        new_user = self.view.add_user_num()
+                        new_pin = self.view.add_user_pin()
+                        print(new_user, new_pin)
+                    
+                    # Deleting User
+                    elif p2 == 3:
+                        u_id = self.view.user_id
+                        res = self.view.del_user(u_id)
+
+                    # Return to previous setting
+                    elif p2 == 0:
+                        pass
+
+
+        except ValueError:
+            print(self.view.error(str(ValueError)))
--- views/cmd_interface/cmd_views.py
+++ views/cmd_interface/cmd_views.py
@@ -1,96 +1,68 @@
 import getpass
+import os
 
 class CommandLinePrompts:
     """ 
     
-    This is file that contains all the prompts displayed on the command line interface.
-        
+    Prompts that are displayed on the command line interface.
+
     """
 
     
     # Level 0 Menu
     def login_id(self):
-        return input("\nID: ")
+        os.system('cls')
+        return input("\nLogin: ")
 
     def login_passwd(self):
         return getpass.getpass("Password: ")
 
     # Level 1 Menu
     def main_prompt(self):
-        return input("\n\
-                      Bank Manager\n\
-                      \n\
-                      1.) Manage Users\n\
-                      2.) Logout\n\
-                      \n\
-                      Enter Command: ")
+        os.system('cls')
+        return int(input("\nBank Manager\n\n1.) Manage Users\n2.) Logout\n\nEnter Command: "))
 
     # Level 2 Menu
     def manage_users(self):
-        return input("\n\
-                      Manage Users\n\
-                      \n\
-                      1.) Configure Existing User\n\
-                      2.) Add User\n\
-                      3.) Delete User\n\
-                      \n\
-                      7.) Return\n\
-                      \n\
-                      Enter Command: ")
+        os.system('cls')
+        return int(input("\nManage Users\n\n1.) Configure Existing User\n2.) Add User\n3.) Delete User\n\n0.) Return\n\nEnter Command: "))
 
     # Level 3 Menus
     def add_user_num(self):
+        os.system('cls')
         return input("\nNew Bank Number: ")
 
     def add_user_pin(self):
-        return getpass.getpass("\nNew PIN: ")
+        return getpass.getpass("New PIN: ")
 
     def del_user(self, user_id):
-        return input("Delete {}? (Y/N)".format(user_id))
+        os.system('cls')
+        return str(input("Delete {}? (Y/N)".format(user_id)))
 
     def user_id(self):
+        os.system('cls')
         return input("User ID: ")
 
     def config_user(self, user_id):
-        return input("\n\
-                     Configuring {}\n\
-                     \n\
-                     1.) Manage Accounts\n\
-                     2.) Transaction Logs\n\
-                     3.) Delete User\n\
-                     \n\
-                     7.) Return\n\
-                      \n\
-                      Enter Command: ".format(user_id))
+        os.system('cls')
+        return int(input("\nConfiguring {}\n\n1.) Manage Accounts\n2.) Transaction Logs\n3.) Delete User\n\n0.) Return\n\nEnter Command: ".format(user_id)))
 
     # Level 4 Menus
     def manage_accounts(self):
-        return input("\n\
-                     Manage Accounts\n\
-                     \n\
-                     1.) Display Accounts\n\
-                     2.) Add Account\n\
-                     3.) Delete Account\n\
-                     \n\
-                     7.) Return\n\
-                      \n\
-                      Enter Command: ")
+        os.system('cls')
+        return int(input("\nManage Accounts\n\n1.) Display Accounts\n2.) Add Account\n3.) Delete Account\n\n0.) Return\n\nEnter Command: "))
 
     # Level 5 Menu
     def display_accounts(self, accounts):
+        os.system('cls')
         print("\nManage Accounts\n")
         for account in accounts:
             print(str(accounts.index(account)) + ".)", account)
-        return input("\n\nEnter Command: ")
+        return int(input("\n\nEnter Command: "))
 
     def accounts(self):
-        input("\n\
-              Accounts\n\
-              \n\
-              1.) Chequing\n\
-              2.) Savings\n\
-              3.) Term Savings\n\
-              \n\
-              7.) Return\n\
-              \n\
-              Enter Command: ")
+        os.system('cls')
+        return input("\nAccounts\n\n1.) Chequing\n\2.) Savings\n\3.) Term Savings\n\n0.) Return\n\nEnter Command: ")
+
+    def error(self, err):
+        return err
