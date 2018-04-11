c1378de57113f34d055689970e42f0b185a7b5d7
jsalisi <jsalisi@gmail.com>
2018-04-10 17:05:04 -0700

    eh

--- cli.py
+++ cli.py
@@ -1,4 +1,9 @@
 # This is the file that runs the Command Line Interface
 
+import getpass
 from controller.cmd_controller import *
 from views.cmd_interface import *
+
+test = getpass.getpass("\nPassword: ")
+
+print("\n", test)
